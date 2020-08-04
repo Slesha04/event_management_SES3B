8bdd3f40441d8e0b216f0069fc3c765b5e96fda5
KnowingDeWay <12886824@student.uts.edu.au>
2020-08-04 16:26:44 +1000

    Created back-end solution

---
+++ back-end/Event_Management_System/Event_Management_System.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Event_Management_System.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ back-end/Event_Management_System/Event_Management_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Event_Management_System.WebApiApplication" Language="C#" %>
