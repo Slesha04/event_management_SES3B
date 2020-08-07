6ce7b2100e4f83fd6da66e27f1c86834333fbc8a
KnowingDeWay <12886824@student.uts.edu.au>
2020-08-08 01:53:56 +1000

    Added new migration for user tokens

---
+++ back-end/Event_Management_Application/Event_Management_Application/Migrations/20200807155232_user-tokens.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Event_Management_Application.Migrations
+{
+    public partial class usertokens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTokenEntries",
+                columns: table => new
+                {
+                    TokenId = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    TokenIssueDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTokenEntries", x => x.TokenId);
+                    table.ForeignKey(
+                        name: "FK_UserTokenEntries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_EventOrganiserId",
+                table: "Events",
+                column: "EventOrganiserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTokenEntries_UserId",
+                table: "UserTokenEntries",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Users_EventOrganiserId",
+                table: "Events",
+                column: "EventOrganiserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Users_EventOrganiserId",
+                table: "Events");
+
+            migrationBuilder.DropTable(
+                name: "UserTokenEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_EventOrganiserId",
+                table: "Events");
+        }
+    }
+}
