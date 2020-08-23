cf3a58e0d99256608570a4965b166f12f0fedcc0
KnowingDeWay <12886824@student.uts.edu.au>
2020-08-23 21:47:04 +1000

    Removed unnecessary constant from System Resources

--- back-end/Event_Management_Application/Event_Management_Application/ResourceManagement/SystemResources.cs
+++ back-end/Event_Management_Application/Event_Management_Application/ResourceManagement/SystemResources.cs
@@ -3,7 +3,7 @@
     public static class SystemResources
     {
         public const string DATABASE_CONNECTION_STRING = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Event_Management_DB;Trusted_Connection=true";
-        public const string TEST_DATABASE_CONNECTION_STRING = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Event_Management_Test_DB;Trusted_Connection=true";
+        public const string PROD_DATABASE_STRING = "";
         public const string TOKEN_SECURITY_KEY = "6170ac834a44388f4f419f48605f887f162fcb1ccdd7c2108bdde362c307096f";
         public const string VALID_ISSUER = "eventmanagementsys";
         public const string VALID_AUDIENCE = "users";
