d8b9ce16ce734d4640ee917bc740b9b71d991f4b
KnowingDeWay <12886824@student.uts.edu.au>
2020-08-24 15:25:38 +1000

    Doing prep work for Azure.

--- back-end/Event_Management_Application/Event_Management_Application/ResourceManagement/SystemResources.cs
+++ back-end/Event_Management_Application/Event_Management_Application/ResourceManagement/SystemResources.cs
@@ -3,7 +3,8 @@
     public static class SystemResources
     {
         public const string DATABASE_CONNECTION_STRING = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Event_Management_DB;Trusted_Connection=true";
-        public const string PROD_DATABASE_STRING = "";
+        public const string AZURE_ADMIN_ACCOUNT_PASS = "eventmanagement124!";
+        public const string PROD_DATABASE_STRING = "Server=tcp:event-management-applicationdbserver.database.windows.net,1433;Initial Catalog=Event_Management_Application_db;Persist Security Info=False;User ID=sysadmin;Password=eventmanagement124!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public const string TOKEN_SECURITY_KEY = "6170ac834a44388f4f419f48605f887f162fcb1ccdd7c2108bdde362c307096f";
         public const string VALID_ISSUER = "eventmanagementsys";
         public const string VALID_AUDIENCE = "users";
