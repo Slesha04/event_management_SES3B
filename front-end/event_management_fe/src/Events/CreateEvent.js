71a14e5523ec293debaf9ec29827f2787a49d6cf
MichaelFedulov <Mikhail_Fedulov@hotmail.com>
2020-08-24 19:39:51 +1000

    Added initial create event page
    Still need to fix styling and form fillds

--- front-end/event_management_fe/src/Events/CreateEvent.js
+++ front-end/event_management_fe/src/Events/CreateEvent.js
@@ -10,6 +10,10 @@ import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 /*
 const CreateEvent =(props) =>{
@@ -78,9 +82,84 @@ const useStyles = makeStyles((theme) => ({
 const CreateEvent =(props) =>{
   const classes = useStyles();
 
-
-
   return (
+      <React.Fragment>
+       <Typography variant="h6" gutterBottom>
+         Shipping address
+       </Typography>
+       <Grid container spacing={3}>
+         <Grid item xs={12} sm={6}>
+           <TextField
+             required
+             id="firstName"
+             name="firstName"
+             label="Event Name"
+             fullWidth
+             autoComplete="given-name"
+           />
+         </Grid>
+         <Grid item xs={12} sm={6}>
+           <TextField
+             required
+             id="lastName"
+             name="lastName"
+             label="Event Type"
+             fullWidth
+             autoComplete="family-name"
+           />
+         </Grid>
+         <Grid item xs={12}>
+           <TextField
+             required
+             id="address1"
+             name="address1"
+             label="Describtion"
+             fullWidth
+             autoComplete="shipping address-line1"
+           />
+         </Grid>
+         <Grid item xs={12}>
+           <TextField
+             id="address2"
+             name="address2"
+             label="Event Notes"
+             fullWidth
+             autoComplete="shipping address-line2"
+           />
+         </Grid>
+         <Grid item xs={12} sm={6}>
+           <TextField
+             required
+             id="city"
+             name="city"
+             label="Event city"
+             fullWidth
+             autoComplete="shipping address-level2"
+           />
+         </Grid>
+         <Grid item xs={12} sm={6}>
+           <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+         </Grid>
+         <Grid item xs={12} sm={6}>
+           <TextField
+             required
+             id="zip"
+             name="zip"
+             label="Zip / Postal code"
+             fullWidth
+             autoComplete="shipping postal-code"
+           />
+         </Grid>
+         <Grid item xs={12}>
+           <FormControlLabel
+             control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+             label="I confirm the details above"
+           />
+         </Grid>
+       </Grid>
+     </React.Fragment>
+
+/*
     <React.Fragment>
       <CssBaseline />
       <AppBar position="absolute" color="default" className={classes.appBar}>
@@ -107,7 +186,7 @@ const CreateEvent =(props) =>{
         </Paper>
         <Copyright />
       </main>
-    </React.Fragment>
+    </React.Fragment>*/
   );
 }
 
