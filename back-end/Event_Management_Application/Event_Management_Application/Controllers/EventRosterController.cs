07f53bc4a712f6bf96ddad56908a52bbe427eb1a
Matthew Delotavo <matthew.t.delotavo@student.uts.edu.au>
2020-09-18 20:51:43 +1000

    :sparkles: Implement MarkAttendeeSelf route and test method

--- back-end/Event_Management_Application/Event_Management_Application/Controllers/EventRosterController.cs
+++ back-end/Event_Management_Application/Event_Management_Application/Controllers/EventRosterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Event_Management_Application.DataAccess;
 using Event_Management_Application.Models;
+using Event_Management_Application.ResourceManagement;
 using Event_Management_Application.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -61,7 +62,24 @@ namespace Event_Management_Application.Controllers
         [Authorize]
         public ActionResult MarkAttendeeSelf([FromRoute] string inputCode, [FromRoute] int eventId)
         {
-            throw new NotImplementedException();
+            var tokenEntry = _tokenManager.ValidateAndReturnTokenEntry(_tokenManager.ExtractToken(Request));
+            if (tokenEntry != null)
+            {
+                //var entry = _dbContext.EventRosterEntries.Where(x => tokenEntry.User.UserId == x.AttendeeId && x.EventId == eventId && x.InputCode == inputCode).FirstOrDefault();
+                var entry = _dbContext.EventRosterEntries.Where(x => x.EventId == eventId && x.InputCode == inputCode).FirstOrDefault();
+
+                if (entry == null)
+                {
+                    return BadRequest("No event roster entry corresponding to event Id");
+                }
+
+                entry.AttendeeArrived = true;
+
+                _dbContext.SaveChanges();
+
+                return Ok();
+            }
+            return StatusCode(401, SystemResources.INVALID_TOKEN_MESSAGE);
         }
     }
 }
