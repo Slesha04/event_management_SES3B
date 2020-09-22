c9295b755436c54ea86c43f6c94110fb945d41ab
slesha04 <sleshamishra4@gmail.com>
2020-09-22 12:48:21 +1000

    Responsive cards

--- front-end/event_management_fe/src/Shared/Cards.js
+++ front-end/event_management_fe/src/Shared/Cards.js
@@ -9,7 +9,28 @@ const Cards = () => {
             <div className="cards__container">
                 <div className= "cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem src="images/img-9.jpg"
+                        text="Explore the Magical Waterfalls at Wenthworth falls"
+                        label="Adventure"
+                        path="/all-events"/>
+                        <CardItem src="images/img-7.jpg"
+                        text="PhotoSoc Photography Bootcamp"
+                        label="Photography"
+                        path="/all-events"/>
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem src="images/img-2.jpg"
+                        text="Helocopter ride on top of the barreir reef"
+                        label="Adventure"
+                        path="/all-events"/>
+                        <CardItem src="images/img-1.jpg"
+                        text="Campint through the Blue Mountains"
+                        label="Photography"
+                        path="/all-events"/>
+                        <CardItem src="images/img-5.jpg"
+                        text="Banking 101"
+                        label="Finance"
+                        path="/all-events"/>
                     </ul>
                 </div>
             </div>
