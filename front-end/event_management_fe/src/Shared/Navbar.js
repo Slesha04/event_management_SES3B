ff1d44723a9d7fe7905d4c4800772cd5476c34dc
slesha04 <sleshamishra4@gmail.com>
2020-09-22 10:24:02 +1000

    CSS for HP

--- front-end/event_management_fe/src/Shared/Navbar.js
+++ front-end/event_management_fe/src/Shared/Navbar.js
@@ -1,20 +1,57 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css'
 
 function Navbar() {
     const [click, setClick]= useState(false);
+    const [button,setButton] = useState(true)
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false)
+        }else{
+            setButton(true)}
+    };
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
-                    UTS:EVENTS <i class="fas fa-glass-cheers"></i>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                    UTS:EVENTS <i class="fas fa-glass-cheers"/>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/global-chat' className='nav-links' onClick={closeMobileMenu}>
+                            Chat
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/profile' className='nav-links' onClick={closeMobileMenu}>
+                            Profile
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign In
+                        </Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
         
