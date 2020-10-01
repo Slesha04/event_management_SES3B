b30b9dc5ef76d49f457c1a89b5035440d9f836f5
KnowingDeWay <12886824@student.uts.edu.au>
2020-10-01 16:02:37 +1000

    Added startup functionality to add a global channel if it were not present.

--- back-end/Event_Management_Application/Event_Management_Application/Program.cs
+++ back-end/Event_Management_Application/Event_Management_Application/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Event_Management_Application.ResourceManagement;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +16,10 @@ namespace Event_Management_Application
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
+            // Runs tasks that need to be ran before performing any operations
+            StartupTasksManager manager = new StartupTasksManager();
+            manager.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
