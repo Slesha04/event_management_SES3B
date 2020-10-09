643ce1b7e3aa35bf5fb5f0b32771e8739d000678
aviattri <avirajattri8@gmail.com>
2020-10-10 05:17:15 +1100

    added searchbar ui

--- front-end/event_management_fe/src/Events/Allevents.js
+++ front-end/event_management_fe/src/Events/Allevents.js
@@ -6,7 +6,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { getHeaderToken, getToken, getUserID } from "../Login/JwtConfig";
 import { getUserName } from "../Login/JwtConfig";
- import { getUserPlatformAPIPort } from "../Login/JwtConfig";
+import { getUserPlatformAPIPort } from "../Login/JwtConfig";
+import SearchBar from "material-ui-search-bar";
+
 const ProjectData = [
   { imageUrl: require("./dummyData/img1.jpg") },
   {
@@ -37,9 +39,46 @@ const ProjectData = [
     imageUrl: require("./dummyData/img11.jpg"),
   },
 ];
+
 const AllEvents = () => {
   const [post, setPostArray] = useState([]);
-   var x ;
+  const [searchItem, setSearchItem] = useState("");
+  var x;
+
+  const handleSearchItem = (event) => {
+    setSearchItem(event.target.value);
+  };
+
+  const handleRequest = (event) => {
+    console.log(searchItem);
+    axios
+      .get(
+        `${getUserPlatformAPIPort()}api/EventController/SearchEventsByName/${searchItem}/1`
+      )
+      .then(
+        (res) => {
+          if (res.status === 200) {
+            // 1. Make a shallow copy of the items
+            let arr = post;
+            // 2. Make a shallow copy of the item you want to mutate
+            console.log(res.data);
+
+            let item = res.data;
+            // 3. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+            arr[1] = item;
+            // 4. Set the state to our new copy
+            setPostArray(arr);
+            console.log(post);
+            // console.log(res.data[0].eventTitle);
+          }
+        },
+        (error) => {
+          alert("something Went Wrong");
+          console.log(error);
+        }
+      );
+  };
+
   useEffect(() => {
     const userId = getUserID();
     console.log("called");
@@ -47,7 +86,6 @@ const AllEvents = () => {
       .get(
         `${getUserPlatformAPIPort()}api/EventController/LoadMostPopularEvents/1`
       )
-
       .then(
         (res) => {
           if (res.status === 200) {
@@ -63,6 +101,7 @@ const AllEvents = () => {
   const imgData = (event) => {
     return ProjectData[Math.floor(Math.random() * ProjectData.length)].imageUrl;
   };
+
   const events = [
     {
       start: "2015-07-20",
@@ -82,6 +121,13 @@ const AllEvents = () => {
   return (
     <div className="card">
       <h1>Check out all most popular events happening on campus</h1>
+
+      <SearchBar
+        value={searchItem}
+        onChange={(searchItem) => setSearchItem(searchItem)}
+        onRequestSearch={() => handleRequest(searchItem)}
+      />
+
       {post.map(
         (item) => (
           (x = imgData()),
@@ -92,7 +138,7 @@ const AllEvents = () => {
                 <div className="card__wrapper">
                   <ul className="card__items">
                     <CardItem
-                      src= {x}
+                      src={x}
                       text={item.eventTitle}
                       bodyText={item.bodyText}
                       label="Adventure"
@@ -105,7 +151,7 @@ const AllEvents = () => {
           )
         )
       )}
-     </div>
+    </div>
   );
 };
 
